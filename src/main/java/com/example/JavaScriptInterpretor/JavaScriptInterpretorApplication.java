d32d433f0c1490056c669a317cdd13ab3067a920
tjp824 <tjp824@gmail.com>
2017-12-08 15:34:00 -0500

    First Commit

---
+++ src/main/java/com/example/JavaScriptInterpretor/JavaScriptInterpretorApplication.java
@@ -0,0 +1,12 @@
+package com.example.JavaScriptInterpretor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScriptInterpretorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaScriptInterpretorApplication.class, args);
+	}
+}
