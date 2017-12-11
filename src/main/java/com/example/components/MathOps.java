6b64e38d5d8c18ffff71364cdf19ba8d300944eb
tjp824 <tjp824@gmail.com>
2017-12-11 14:28:35 -0500

    Fun with Annotations: fields and objects now automatically added in

--- src/main/java/com/example/components/MathOps.java
+++ src/main/java/com/example/components/MathOps.java
@@ -1,4 +1,20 @@
 package com.example.components;
 
+import com.example.annotations.JSComponent;
+import com.example.annotations.JSRunnable;
+
+@JSComponent(name = "math")
 public class MathOps {
+
+    @JSComponent
+    public double pi = 3.141592658979323846;
+
+    @JSRunnable
+    public double add(double a, double b) {return a+b;}
+    @JSRunnable
+    public double subt(double a, double b) {return a-b;}
+    @JSRunnable
+    public double mult(double a, double b) {return a*b;}
+    @JSRunnable
+    public double div(double a, double b) {return a/b;}
 }
