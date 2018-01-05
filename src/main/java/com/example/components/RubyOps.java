12b0aa56aa76ffa484a859caf19c9132410d5c0d
tjp824 <tjp824@gmail.com>
2018-01-05 12:03:19 -0500

    Forgot to push this up with the last update

--- src/main/java/com/example/components/RubyOps.java
+++ src/main/java/com/example/components/RubyOps.java
@@ -1,4 +1,20 @@
 package com.example.components;
 
+import com.example.annotations.JSComponent;
+import com.example.annotations.JSRunnable;
+import com.example.runners.RubyRunner;
+
+@JSComponent(name = "ruby")
 public class RubyOps {
+
+    @JSRunnable
+    public String run(String fname) {
+        return RubyRunner.getRunner().runFile(fname);
+    }
+
+    @JSRunnable
+    public String exec(String script) {
+        return RubyRunner.getRunner().run(script);
+    }
+
 }
