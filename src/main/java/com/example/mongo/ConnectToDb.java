061f5650fcec5a58a96321306d7106f2d0432a45
BONDS <ab8201@att.com>
2018-01-08 12:02:26 -0500

    Working on data insertion

--- src/main/java/com/example/mongo/ConnectToDb.java
+++ src/main/java/com/example/mongo/ConnectToDb.java
@@ -2,75 +2,65 @@ package com.example.mongo;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Iterator;
-import java.util.logging.Logger;
 
+import java.util.logging.Logger;
 
 import org.bson.Document;
 
-import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
-import com.mongodb.MongoClientOptions.Builder;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Updates;
 
 public class ConnectToDb {
-	
+
 	private static Logger log;
 
 	public static void main(String[] args) {
 		log = Logger.getLogger(ConnectToDb.class.getName());
-		// Creating a Mongo client 
-		
+		// Creating a Mongo client
+
 		InetAddress ip;
 		String ipAddress = "";
 		try {
 			ip = InetAddress.getLocalHost();
 			ipAddress = ip.getHostAddress();
 			log.info(ipAddress);
-			
-		}catch(UnknownHostException e) {
+
+		} catch (UnknownHostException e) {
 			log.warning(e.getMessage());
 		}
-		
-		MongoClient mongo = new MongoClient(new ServerAddress(ipAddress, 27017), new MongoClientOptions.Builder().build());
 
-		// Creating Credentials 
-		MongoCredential credential; 
-		credential = MongoCredential.createCredential("sampleUser", "myDb", 
-				"password".toCharArray()); 
-		log.info("Connected to the database successfully");  
+		MongoClient mongo = new MongoClient(new ServerAddress(ipAddress, 27017),
+				new MongoClientOptions.Builder().build());
 
-		//Accessing the database 
-		MongoDatabase database = mongo.getDatabase("myDb");  
-	    System.out.println("Credentials ::"+ credential); 
-		//Creating a collection 
-	    log.info("Collection created successfully");
-	    
-	    for(String name: database.listCollectionNames()) {
-	    	System.out.println(name);
-	    }
-	    
-	    MongoCollection<Document> collection = database.getCollection("sampleCollection");
-	    log.info("Collection samepleCollect selected successfully");
-		
-	    
-//	   MongoCollection<Document> collection2 = database.getCollection("sampleCollection2");
-//	   collection2.drop();
-//	   log.info("Collection successfully dropped...");
-	   
+		// Creating Credentials
+		MongoCredential credential;
+		credential = MongoCredential.createCredential("sampleUser", "myDb", "password".toCharArray());
+		log.info("Connected to the database successfully");
 
-	   
-	   mongo.close();
+		// Accessing the database
+		MongoDatabase database = mongo.getDatabase("myDb");
+		System.out.println("Credentials ::" + credential);
+		// Creating a collection
+		log.info("Collection created successfully");
 
-		
-		
+		for (String name : database.listCollectionNames()) {
+			System.out.println(name);
+		}
+
+		MongoCollection<Document> collection = database.getCollection("sampleCollection");
+		log.info("Collection samepleCollect selected successfully");
+
+		// MongoCollection<Document> collection2 =
+		// database.getCollection("sampleCollection2");
+		// collection2.drop();
+		// log.info("Collection successfully dropped...");
+
+		mongo.close();
+
 	}
 
 }
---
+++ src/main/java/com/example/mongo/RandomValueGenerator.java
@@ -0,0 +1,74 @@
+package com.example.mongo;
+
+import java.util.Random;
+import java.util.Timer;
+
+import org.apache.log4j.Logger;
+import org.bson.Document;
+
+import com.github.javafaker.Faker;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class RandomValueGenerator {
+
+	
+	private static Logger log;
+	
+	
+	public static void run() {
+		log = Logger.getLogger(RandomValueGenerator.class.getName());
+		MongoClient mongo = getClient();
+		MongoDatabase database = mongo.getDatabase("myDB");
+		log.info("Connected to Database");
+		
+		MongoCollection<Document> collection = database.getCollection("Test");
+		collection.insertOne(populateDatabase());
+		log.info(collection.toString());
+		
+	}
+	
+	public static Document populateDatabase() {
+		Faker faker = new Faker();
+		Random random = new Random();
+		String firstname = faker.name().firstName();
+		String lastname = faker.name().lastName();
+		int age = random.nextInt(120);
+		String state = getState();
+		int balance = random.nextInt(1000000);
+		
+		Document document = new Document();
+		document.put("First Name", firstname);
+		document.put("Last Name", lastname);
+		document.put("Age", age);
+		document.put("Living State", state);
+		document.put("Balance", balance);
+		
+		
+		
+		return document;
+	}
+	
+	
+	public static String getState() {
+		String[] states = {"AL", "AK", "AZ", "AR", "CA", "CO","CT","DE","FL","GA","HI","ID","IL"
+				,"IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ"
+				,"NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"};
+		Random random = new Random();
+		int statePicker = random.nextInt(49);
+		String selection = new String(states[statePicker]);
+		return selection;
+	}
+	
+	public static MongoClient getClient() {
+		return new MongoClient(new ServerAddress("192.168.99.100", 27017), new MongoClientOptions.Builder().build());
+	}
+	public static void main(String[] args) {
+		RandomValueGenerator.run();
+	}
+	
+	
+}
